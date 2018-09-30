9380c410ce58e369f5440e9ba8babc41d6549ef7
Raman Yasel <strogofcdm@gmail.com>
2018-09-30 22:10:11 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let currentNumber;
+  let col;
+  let g;
+  let count = number;
+  let time_var = base;
+  for (let i = 2; i <= base; i++) {
+    if (time_var % i === 0) {
+      currentNumber = 0;
+      while (time_var % i === 0) {
+        time_var = Math.floor(time_var / i);
+        currentNumber++;
+      };
+      col = 0;
+      g = number;
+      while (g / i > 0) {
+        col += Math.floor(g / i);
+        g = Math.floor(g / i);
+      };
+      var timeVar = Math.floor(col / currentNumber);
+      if (count > timeVar) {
+        count = timeVar;
+      }
+    }
+  }
+
+  return count;
 }
